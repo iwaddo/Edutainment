a402a7388ce1a4416cb150092b106e32854cc28e
Ian Waddington <iwaddo@icloud.com>
2022-12-27 15:41:33 +0000

    Initial Commit

---
+++ Edutainment2/Edutainment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Edutainment2App.swift
+//  Edutainment2
+//
+//  Created by Ian Waddington on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Edutainment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
